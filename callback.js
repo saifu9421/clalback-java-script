394326fbd63cd9de49bd8512a83a33d9aacbbf54
saifu9421 <saifursaif905@gmail.com>
2023-09-05 00:00:54 +0600

    added a callback fubnction

--- callback.js
+++ callback.js
@@ -1,9 +1,24 @@
     
-    const function1 = (callback) =>{
-        console.log(typeof callback);
-       callback();
+
+    // callback function : ackta function parameter arackta function ke pass kra 
+
+    const function1 = (a,b,callback) =>{
+    
+        // console.log(typeof callback);
+       callback(a,b);
     };
-    const callback = ()=>{
-        console.log("I am callback function");
+
+    const callback = (x,y)=>{
+          console.log("Sum = ", x+y ); 
     };
-    function1(callback);
+    function1(10,5,callback);
+
+
+
+    const function2 =(a,b,c, callback) =>{
+         callback(a,b,c);
+    };
+    const add =  (x,y,z) =>{
+        console.log("Sum = ", x*y*z);
+    };
+     function2(10,10,10,add);
